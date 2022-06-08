28c831fa1e3155c551cb6e01a7aea34c99743f41
xm3854 <kxm3854@gmail.com>
2022-06-08 13:17:04 +0800

    MVP done

---
+++ Project1Code/script.js
@@ -0,0 +1,97 @@
+const cells = Array.from(document.querySelectorAll(".cell"));
+const enemyCells = cells.slice(0, 22);
+const playerCells = cells.slice(20, 22);
+const scoreDisplay = document.querySelector(".score");
+
+let score;
+
+function reset() {
+  score = 0;
+  scoreDisplay.innerHTML = "0";
+
+  cells.forEach(function eraseCell(cell) {
+    return (cell.innerHTML = "");
+  });
+  spawnPlayer();
+  spawnEnemy();
+}
+
+function playerMove(e) {
+  const player = document.querySelector(".player");
+
+  if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
+    moveEnemy();
+    spawnEnemy();
+    if (
+      e.key === "ArrowRight" &&
+      playerCells.includes(
+        player.parentElement.nextElementSibling.nextElementSibling
+      )
+    ) {
+      player.parentElement.nextElementSibling.nextElementSibling.appendChild(
+        player
+      );
+    }
+    if (
+      e.key === "ArrowLeft" &&
+      playerCells.includes(
+        player.parentElement.previousElementSibling.previousElementSibling
+      )
+    ) {
+      player.parentElement.previousElementSibling.previousElementSibling.appendChild(
+        player
+      );
+    }
+    checkGame();
+    score++;
+    scoreDisplay.innerHTML = score;
+  }
+}
+
+function spawnEnemy() {
+  if (score % 2 !== 0) {
+    const position = Math.floor(Math.random() * 2);
+    enemyCells[position].innerHTML = "<div class='enemy'></div>";
+  }
+}
+
+function spawnPlayer() {
+  const position = Math.floor(Math.random() * 2);
+  playerCells[position].innerHTML = "<div class='player'></div>";
+}
+
+function moveEnemy() {
+  for (let i = enemyCells.length - 1; i >= 0; i--) {
+    //only moving enemy cell, not moving player cell
+    //if include enemy cell, move, if not dont move
+    if (
+      enemyCells[i].children.length > 0 &&
+      enemyCells[i].children[0].className == "enemy"
+    ) {
+      if (i >= enemyCells.length - 2) {
+        enemyCells[i].removeChild(enemyCells[i].children[0]);
+      } else {
+        const cell = enemyCells[i];
+        const nextCell = cells[i + 2];
+        const enemy = cell.children[0];
+        nextCell.appendChild(enemy);
+      }
+    }
+  }
+}
+
+function checkGame() {
+  let stopGame = false;
+  for (let i = 0; i < playerCells.length; i++) {
+    if (playerCells[i].children.length > 1) {
+      stopGame = true;
+      if (stopGame) {
+        alert(`Gameover, your score is ${score}`);
+        reset();
+      }
+    }
+  }
+}
+
+reset();
+document.addEventListener("keydown", playerMove);
